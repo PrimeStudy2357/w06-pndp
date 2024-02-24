3a3d130e589a8792992cf9297a935a3ad3face3d
PndP2357 <ssy@sosuyeob-ui-MacBookAir.local>
2024-02-24 16:14:56 +0900

    w06-example01

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const a = 5;
+
+  const b = a + 5;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.<br />
+          Hello World!<br />
+          { b }<br />
+          Lee Ji-hoon GAP JIL Go bal hapnida
         </p>
         <a
           className="App-link"
